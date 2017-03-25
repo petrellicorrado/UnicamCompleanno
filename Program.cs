f091b16cadf8d7bd9180734229e5c72838daea36
petrellicorrado <corrado.petrelli@studenti.unicam.it>
2017-03-25 16:24:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace UnicamCompleanno
 {
@@ -11,35 +11,44 @@ namespace UnicamCompleanno
             string testoDigitatoDallUtente = Console.ReadLine();
 
             try {
+                //Trasformo la stringa in oggetto di tipo DateTime
                 DateTime dataDiNascita = DateTime.Parse(testoDigitatoDallUtente);
+               
+                //Se oggi è il compleanno
+                if(dataDiNascita.Equals(DateTime.Today))
+                    Console.WriteLine("Tanti Auguri!");
+                else{
 
-                //Calcoliamo gli anni dell'utente.
-                //Quando non sappiamo come si fa o quando, come in questo caso,
-                //.NET non ha un modo facile per ottenere la differenza in anni
-                //andiamo a vedere su StackOverlow se qualcuno ha già avuto la stessa necessità
-                //Il metodo Years è stato preso da lì (è definito più in basso, in questo file)
-                string anniDiDifferenza = Years(dataDiNascita, DateTime.Today);
-                Console.WriteLine($"Oggi hai {anniDiDifferenza} anni");
-
-                //Ora calcoliamo i giorni che ci separano dal prossimo compleanno
-                DateTime dataProssimoCompleanno = new DateTime(DateTime.Today.Year, dataDiNascita.Month, dataDiNascita.Day);
-                TimeSpan differenzaTemporale = dataProssimoCompleanno - DateTime.Today;
-                double differenzaInGiorni = differenzaTemporale.TotalDays;
-                Console.WriteLine($"Restano {differenzaInGiorni} giorni al tuo prossimo compleanno!");
-
+                    //Se la data è successiva ad oggi
+                    if(dataDiNascita > DateTime.Today)
+                        Console.WriteLine("Data successiva ad oggi!");
+                    else{
+                         //Memorizzo la giornata di oggi
+                        DateTime oggi = DateTime.Today;
+                        //Sottranno l'anno corrente per l'anno di nascita
+                        int anniDiNascita = oggi.Year - dataDiNascita.Year;
+                        //Se non hai compiuto ancora gli anni
+                        if( dataDiNascita > oggi.AddYears(-anniDiNascita))
+                            anniDiNascita--;
+                        Console.WriteLine("Hai {0} anni",anniDiNascita);
+                        
+                        //NOTA BENE: NON FUNZIONA PER ANNI BISESTILI!
+                        //Calcolo la data del prossimo compleanno
+                        DateTime dataProssimoCompleanno = dataDiNascita.AddYears(oggi.Year - dataDiNascita.Year);
+                        //Se ho già compiuto gli anni quest'anno
+                        if (dataProssimoCompleanno < oggi)
+                            dataProssimoCompleanno = dataProssimoCompleanno.AddYears(1);
+                        //Calcolo il numero di giorni che mancano al mio compleanno
+                        int numeroGiorni = (dataProssimoCompleanno - oggi).Days;                        
+                        Console.WriteLine("Restano {0} giorni al tuo prossimo compleanno!",numeroGiorni);
+               
+                    }
+                }
+               
             } catch (Exception exc) {
                 Console.WriteLine($"Non hai digitato una data valida, il programma ora terminera'. L'errore e' stato: {exc.Message}.");
             }
             Console.ReadKey();
-        }
-
-        //Grazie, StackOverflow!
-        //http://stackoverflow.com/questions/4127363/date-difference-in-years-using-c-sharp#answer-4127477
-        static int Years(DateTime start, DateTime end)
-        {
-            return (end.Year - start.Year - 1) +
-                (((end.Month > start.Month) ||
-                ((end.Month == start.Month) && (end.Day >= start.Day))) ? 1 : 0);
         }
     }
 }
